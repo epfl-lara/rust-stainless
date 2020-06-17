a64bc70ad4c5418096143fb93a3b4da27dcea4cf
Georg Schmid <georg.schmid@epfl.ch>
2020-06-17 11:55:47 +0200

    Add support for --example in cargo-stainless

--- stainless_driver/src/bin/cargo-stainless.rs
+++ stainless_driver/src/bin/cargo-stainless.rs
@@ -1,9 +1,17 @@
+extern crate clap;
 extern crate serde_json;
 
-use serde_json::Value;
+use clap::{App, Arg};
+use serde_json::{Map, Value};
 use std::collections::HashMap;
-use std::process::*;
 use std::path::PathBuf;
+use std::process::*;
+
+#[derive(Debug)]
+struct Config {
+  debug: bool,
+  example_opt: Option<String>,
+}
 
 #[derive(Debug)]
 struct Build {
@@ -29,7 +37,7 @@ fn unpack_name(value: &Value) -> String {
 fn unpack_build_env(value: &Value) -> HashMap<String, String> {
   if let Value::Object(env) = value {
     let mut map: HashMap<String, String> = HashMap::with_capacity(env.len());
-    for (k,v) in env {
+    for (k, v) in env {
       if let Value::String(v) = v {
         map.insert(k.clone(), v.clone());
       }
@@ -42,53 +50,88 @@ fn unpack_build_env(value: &Value) -> HashMap<String, String> {
 
 fn unpack_build_args(value: &Value) -> Vec<String> {
   if let Value::Array(args) = value {
-    args.iter().flat_map(|arg| {
-      if let Value::String(arg) = arg {
-        if !arg.starts_with("--error-format") && !arg.starts_with("--json") {
-          Some(arg.clone())
+    args
+      .iter()
+      .flat_map(|arg| {
+        if let Value::String(arg) = arg {
+          if !arg.starts_with("--error-format") && !arg.starts_with("--json") {
+            Some(arg.clone())
+          } else {
+            None
+          }
         } else {
           None
         }
-      } else {
-        None
-      }
-    }).collect()
+      })
+      .collect()
   } else {
     parsing_error("Expected invocation to contain 'env' object".into())
   }
 }
 
 fn find_sysroot(env: &HashMap<String, String>) -> String {
-  let cargo_path = env.get("CARGO").expect("Expected CARGO env to be present in build plan");
+  let cargo_path = env
+    .get("CARGO")
+    .expect("Expected CARGO env to be present in build plan");
   let mut sysroot_path = PathBuf::from(cargo_path);
   sysroot_path.pop();
   sysroot_path.pop();
   sysroot_path.into_os_string().into_string().unwrap()
 }
 
-fn parse_build(data: &[u8]) -> Build {
-  let plan: Value = serde_json::from_reader(data).unwrap_or_else(|e| {
-    parsing_error(format!("{}", e))
-  });
+fn unpack_invocation<'a>(config: &Config, invocations: &'a Vec<Value>) -> &'a Map<String, Value> {
+  let value = match &config.example_opt {
+    Some(example) => invocations
+      .iter()
+      .find(
+        |inv| match (inv.pointer("/target_kind"), inv.pointer("/args")) {
+          (Some(Value::Array(kinds)), Some(Value::Array(args))) => {
+            kinds.iter().any(|kind| match kind {
+              Value::String(kind) => kind == "example",
+              _ => false,
+            }) && args.iter().any(|arg| match arg {
+              Value::String(arg) => arg.contains(&format!("{}.rs", example)),
+              _ => false,
+            })
+          }
+          _ => false,
+        },
+      )
+      .unwrap_or_else(|| parsing_error("Couldn't find target for the specified example".into())),
+    None => invocations
+      .last()
+      .expect("Expected at least one invocation"),
+  };
+  if let Value::Object(invocation) = value {
+    invocation
+  } else {
+    parsing_error("Expected invocation to be an object".into())
+  }
+}
+
+fn parse_build(config: &Config, data: &[u8]) -> Build {
+  let plan: Value =
+    serde_json::from_reader(data).unwrap_or_else(|e| parsing_error(format!("{}", e)));
+  if config.debug {
+    println!(
+      "[Debug] Read the following build plan:\n\n-----{:#}----\n",
+      plan
+    );
+  }
+
   if let Value::Array(invocations) = plan
     .pointer("/invocations")
     .expect("Expected 'invocations' key")
   {
-    if let Value::Object(invocation) = invocations
-      .last()
-      .expect("Expected at least one invocation")
-    {
-      let program = invocation.get("program").unwrap();
-      assert_eq!(program, "rustc");
-      let env = unpack_build_env(invocation.get("env").unwrap());
-      Build {
-        package_name: unpack_name(invocation.get("package_name").unwrap()),
-        sysroot: find_sysroot(&env),
-        env: env,
-        args: unpack_build_args(invocation.get("args").unwrap()),
-      }
-    } else {
-      parsing_error("Expected invocation to be an object".into())
+    let invocation = unpack_invocation(config, invocations);
+    let program = invocation.get("program").unwrap();
+    assert_eq!(program, "rustc");
+    let env = unpack_build_env(invocation.get("env").unwrap());
+    Build {
+      package_name: unpack_name(invocation.get("package_name").unwrap()),
+      sysroot: find_sysroot(&env),
+      env: env,
+      args: unpack_build_args(invocation.get("args").unwrap()),
     }
   } else {
     parsing_error("Expected 'invocations' to be an array".into())
@@ -96,25 +139,68 @@ fn parse_build(data: &[u8]) -> Build {
 }
 
 fn main() -> ! {
+  let matches = App::new("cargo-stainless")
+    .version("0.0.1")
+    .author("Georg Schmid <georg.schmid@epfl.ch>")
+    .about("A cargo subcommand for rust-stainless extraction")
+    .arg(
+      Arg::with_name("dummy")
+        .help("Dummy argument passed by cargo (the subcommand name)")
+        .required(true)
+        .index(1),
+    )
+    .arg(
+      Arg::with_name("example")
+        .long("example")
+        .takes_value(true)
+        .help("Try to extract an example of the crate instead of the last target"),
+    )
+    .arg(
+      Arg::with_name("debug")
+        .long("debug")
+        .help("Print the read build plan and the chosen target"),
+    )
+    .get_matches();
+
+  let config = Config {
+    debug: matches.is_present("debug"),
+    example_opt: matches.value_of("example").map(|s| s.into()),
+  };
+
   // Parse build plan
-  let build_output = Command::new("cargo")
+  let mut build_cmd = Command::new("cargo");
+  build_cmd
     .arg("build")
     .arg("-Z")
     .arg("unstable-options")
-    .arg("--build-plan")
+    .arg("--build-plan");
+  if config.example_opt.is_some() {
+    build_cmd.arg("--all-targets");
+  }
+
+  let build_output = build_cmd
     .output()
     .expect("Couldn't start cargo build to extract plan");
   if !build_output.status.success() {
     parsing_error("Failed to run cargo build to extract plan".into());
   }
 
-  // NOTE: We currently just pick the last build invocation, which should
-  // work well enough for simple, single target builds.
-  let build = parse_build(&build_output.stdout);
+  // NOTE: When no example is specified we currently just pick the last build
+  // invocation, which should work well enough for simple, single target builds.
+  let build = parse_build(&config, &build_output.stdout);
 
   // Run extraction
-  // println!("Parsed build plan:\n{:#?}", build);
-  println!("cargo-stainless: Found target {}.\n", build.package_name);
+  if config.debug {
+    println!("Parsed build plan:\n{:#?}", build);
+  }
+
+  match config.example_opt {
+    Some(example) => println!("cargo-stainless: Found example target '{}'.\n", example),
+    None => println!(
+      "cargo-stainless: Found default target '{}'.\n",
+      build.package_name
+    ),
+  }
 
   let status = Command::new("rustc_to_stainless")
     .envs(build.env.iter())
