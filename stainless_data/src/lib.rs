5a5f6bc43a31174855d5591008093f04e49ac27e
Romain Ruetschi <github@romac.me>
2020-06-18 00:11:12 +0200

    Allow clippy warnings occuring in generated code

--- stainless_data/src/lib.rs
+++ stainless_data/src/lib.rs
@@ -1,2 +1,10 @@
+#![allow(
+    clippy::needless_lifetimes,
+    clippy::mut_from_ref,
+    clippy::new_without_default,
+    clippy::match_single_binding,
+    clippy::too_many_arguments
+)]
+
 pub mod ast;
 pub mod ser;
