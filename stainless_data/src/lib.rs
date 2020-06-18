6a3670d1d8af927ae99b60da763d255841192572
Romain Ruetschi <romain@informal.systems>
2020-06-19 00:06:03 +0200

    Fix formatting

--- stainless_data/src/lib.rs
+++ stainless_data/src/lib.rs
@@ -1,9 +1,9 @@
 #![allow(
-    clippy::needless_lifetimes,
-    clippy::mut_from_ref,
-    clippy::new_without_default,
-    clippy::match_single_binding,
-    clippy::too_many_arguments
+  clippy::needless_lifetimes,
+  clippy::mut_from_ref,
+  clippy::new_without_default,
+  clippy::match_single_binding,
+  clippy::too_many_arguments
 )]
 
 pub mod ast;
