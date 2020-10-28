d42f411caeda00f68ddf1dd67f48616f561d31eb
Yann Bolliger <yann.bolliger@epfl.ch>
2020-10-28 11:02:55 +0100

    Add easy generics for option and list.
    They work out of the box.

--- stainless_frontend/tests/pass/int_option.rs
+++ stainless_frontend/tests/pass/int_option.rs
@@ -1,28 +1,28 @@
 extern crate stainless;
 
-pub enum IntOption {
+pub enum Option<T> {
   None,
-  Some(i32),
+  Some(T),
 }
 
-pub fn unwrap_or_zero(io: IntOption) -> i32 {
-  match io {
-    IntOption::None => 0,
-    IntOption::Some(x) => x,
+pub fn unwrap_or_default<T>(o: Option<T>, default: T) -> T {
+  match o {
+    Option::None => default,
+    Option::Some(x) => x,
   }
 }
 
-pub fn add_to_int_option(io1: IntOption) -> i32 {
-  let io2 = IntOption::None;
-  let io3 = IntOption::Some(3);
-  unwrap_or_zero(io1) + unwrap_or_zero(io2) + unwrap_or_zero(io3)
+pub fn add_to_int_option(io1: Option<i32>) -> i32 {
+  let io2 = Option::None;
+  let io3 = Option::Some(3);
+  unwrap_or_default(io1, 0) + unwrap_or_default(io2, 0) + unwrap_or_default(io3, 0)
 }
 
-pub struct TwoIntOptions {
-  io1: IntOption,
-  io2: IntOption,
+pub struct TwoOptions<T> {
+  io1: Option<T>,
+  io2: Option<T>,
 }
 
-pub fn get_sum(tio: TwoIntOptions) -> i32 {
-  unwrap_or_zero(tio.io1) + unwrap_or_zero(tio.io2)
+pub fn get_sum(tio: TwoOptions<i32>) -> i32 {
+  unwrap_or_default(tio.io1, 0) + unwrap_or_default(tio.io2, 0)
 }
