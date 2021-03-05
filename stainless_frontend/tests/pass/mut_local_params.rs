9550b6f277680f8edf12ae53c509809074259971
Yann Bolliger <yann.bolliger@epfl.ch>
2021-03-05 14:22:20 +0100

    Test for specs on function with mut params.

--- stainless_frontend/tests/pass/mut_local_params.rs
+++ stainless_frontend/tests/pass/mut_local_params.rs
@@ -1,11 +1,18 @@
 #![allow(unused)]
 
 extern crate stainless;
+use stainless::*;
 
 // This should pass because place is only locally mutable.
 pub fn change<'a, T>(mut place: &'a T, new: &'a T) -> &'a T {
   place = new;
   place
+}
+
+#[pre(a > 0)]
+#[post(ret > 0)]
+pub fn with_spec(mut a: i32) -> i32 {
+  a
 }
 
 pub fn main() {
