206a5517b13690b8a46ae33dd069d116edd2cdcc
Yann Bolliger <yann.bolliger@epfl.ch>
2021-03-08 09:35:05 +0100

    Add test case for phantom data.

--- stainless_frontend/tests/extraction_tests.rs
+++ stainless_frontend/tests/extraction_tests.rs
@@ -94,6 +94,7 @@ define_tests!(
   pass: nested_spec,
   pass: nested_spec_impl,
   pass: panic_type,
+  pass: phantom_data,
   pass: spec_on_trait_impl,
   pass: trait_bounds,
   pass: struct_update,
