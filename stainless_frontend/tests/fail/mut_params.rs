195348b84f64d85264a2dc5d64b145e9535b7c70
Yann Bolliger <yann.bolliger@epfl.ch>
2021-03-12 12:30:05 +0100

    Make negative tests specific to one failure.

--- stainless_frontend/tests/fail/mut_params.rs
+++ stainless_frontend/tests/fail/mut_params.rs
@@ -5,13 +5,3 @@ pub fn change<T>(place: &mut T, new: T) -> &mut T {
   *place = new;
   place
 }
-
-pub fn main() {
-  let mut x = 3;
-  let y = 5;
-
-  assert!(x == 3);
-  // This should fail because of the mutable reference.
-  change(&mut x, y);
-  assert!(x == 5)
-}
