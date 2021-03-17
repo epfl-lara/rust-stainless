79c8263f879c2bc17a7ad82721e026fe14a9ba1a
Yann Bolliger <yann.bolliger@epfl.ch>
2021-03-17 11:11:35 +0100

    Return in pattern match.

--- stainless_frontend/tests/pass/return_stmt.rs
+++ stainless_frontend/tests/pass/return_stmt.rs
@@ -25,3 +25,18 @@ pub fn return_when_wanted(arg: u32) {
 
   panic!("will never reach me")
 }
+
+pub fn return_pattern_match(arg: u32) -> i32 {
+  let s = match arg {
+    1 => return 1,
+    i if 3 <= i && i <= 10 => 100000,
+    a => {
+      if a > 1000 {
+        return 2;
+      }
+      10000000
+    }
+  };
+  assert!(s >= 100000);
+  s
+}
