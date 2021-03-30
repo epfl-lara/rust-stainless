d330eadd9c8e0ea36480610409482497dd54c902
Yann Bolliger <yann.bolliger@epfl.ch>
2021-03-30 14:39:51 +0200

    Create seperate test case

--- stainless_frontend/tests/pass/clone_equality.rs
+++ stainless_frontend/tests/pass/clone_equality.rs
@@ -9,12 +9,6 @@ trait Clone {
   fn clone(&self) -> Self;
 }
 
-#[external]
-#[pure]
-#[post(x.clone().eq(&x))]
-#[allow(unused_variables)]
-fn clone_preserves_eq<T: Clone + Equals>(x: T) {}
-
 pub struct Key(String);
 impl Clone for Key {
   fn clone(&self) -> Self {
