a606785d711bd87d38b013111e85fc223dd27060
Yann Bolliger <yann.bolliger@epfl.ch>
2021-04-14 15:37:43 +0200

    Add some doc.

--- stainless_extraction/src/synth.rs
+++ stainless_extraction/src/synth.rs
@@ -1,16 +1,23 @@
 use super::*;
 
+/// This is just a proxy, a mutable reference to the base extractor. The idea of
+/// `Synth` is just to scope some methods under a common namespace, therefore
+/// the struct mainly provides a namespace/scope.
 pub struct Synth<'a, 'l, 'tcx> {
   base: &'a mut BaseExtractor<'l, 'tcx>,
 }
 
 impl<'l, 'tcx> BaseExtractor<'l, 'tcx> {
+  /// Returns a `Synth` which provides access to all methods that synthesise
+  /// items/trees.
   pub fn synth<'a>(&'a mut self) -> Synth<'a, 'l, 'tcx> {
     Synth { base: self }
   }
 }
 
 impl<'b, 'l, 'tcx> BodyExtractor<'b, 'l, 'tcx> {
+  /// Returns a `Synth` which provides access to all methods that synthesise
+  /// items/trees.
   pub fn synth<'a>(&'a mut self) -> Synth<'a, 'l, 'tcx> {
     self.base.synth()
   }
