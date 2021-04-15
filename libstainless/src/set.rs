40188f1dcb5d96cae883fc99ae7d76657a8a8259
Yann Bolliger <yann.bolliger@epfl.ch>
2021-04-15 16:17:15 +0200

    Add tests for set.

--- libstainless/src/set.rs
+++ libstainless/src/set.rs
@@ -1,6 +1,7 @@
 use im::HashSet;
 use std::hash::Hash;
 
+#[derive(Clone)]
 pub struct Set<T> {
   set: HashSet<T>,
 }
@@ -50,5 +51,42 @@ where
 
   pub fn is_subset_of(&self, other: &Set<T>) -> bool {
     self.set.is_subset(&other.set)
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn test_empty() {
+    assert!(!Set::empty().contains(&123));
+    assert!(Set::<i32>::empty().is_subset_of(&Set::empty()));
+    assert!(Set::empty().is_subset_of(&Set::singleton(123)));
+  }
+
+  #[test]
+  fn set_theory() {
+    let s1 = Set::singleton(1).add(2).add(3);
+    let s2 = Set::singleton(1).add(2);
+    let s3 = Set::singleton(3).add(4);
+
+    assert!(!s1.is_subset_of(&s2));
+    assert!(!s1.is_subset_of(&s3));
+    assert!(s2.is_subset_of(&s1));
+    assert!(!s3.is_subset_of(&s1));
+
+    let new = s1.union(s3.clone()).difference(s2.clone());
+    assert!(new.is_subset_of(&s3) && s3.is_subset_of(&new));
+    assert!(s2.intersection(s3).is_subset_of(&Set::empty()))
+  }
+
+  #[test]
+  fn remove_contains() {
+    let set: Set<String> = Set::singleton("foo".into()).add("bar".into());
+    let set = set.difference(Set::singleton("bar".into()));
+    assert!(!set.contains(&"bar".into()));
+    let set = set.difference(Set::singleton("foo".into()));
+    assert!(!set.contains(&"foo".into()))
   }
 }
