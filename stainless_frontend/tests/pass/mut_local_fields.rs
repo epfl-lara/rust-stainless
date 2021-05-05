658923fab46735b6e3a8cddbec973a6462fde550
Yann Bolliger <yann.bolliger@epfl.ch>
2021-05-05 09:43:22 +0200

    Add test case.

--- stainless_frontend/tests/pass/mut_local_fields.rs
+++ stainless_frontend/tests/pass/mut_local_fields.rs
@@ -8,13 +8,19 @@ struct S {
   field: i32,
 }
 
-fn set_field(s: S) -> S {
-  // current work-around for anti-aliasing
-  let mut s = S { ..s };
+fn set_field(mut s: S) -> S {
   s.field = 789;
   s
 }
 
+struct Outer(S);
+
+fn set_inner_field(mut o: Outer) -> Outer {
+  o.0.field = 101112;
+  o
+}
+
+// As control, the same for a primitive type
 fn set_int(mut s: i32) -> i32 {
   s = 1000;
   s
@@ -29,6 +35,10 @@ pub fn main() {
 
   let s = set_field(s);
   assert!(s.field == 789);
+
+  let o = Outer(s);
+  let o = set_inner_field(o);
+  assert!(o.0.field == 101112);
 
   let i = set_int(12);
   assert!(i == 1000);
