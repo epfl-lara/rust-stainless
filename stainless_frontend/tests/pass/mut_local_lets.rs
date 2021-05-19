2fb5f06caf690b43f2a651d9e102247fdec5f99b
Yann Bolliger <yann.bolliger@epfl.ch>
2021-05-19 19:01:30 +0200

    Fresh copy let var initializer. This is safe becaues it consumes whatever is the initializer.

--- stainless_frontend/tests/pass/mut_local_lets.rs
+++ stainless_frontend/tests/pass/mut_local_lets.rs
@@ -2,9 +2,22 @@
 
 extern crate stainless;
 
-pub enum IntOption {
-  None,
-  Some(i32),
+pub fn f<T>(option: Option<T>) {
+  // Again, local mutation
+  let mut option2 = None;
+  let mut option = None;
+  option2 = option;
+  option = Some(123);
+}
+
+pub struct Thing<T> {
+  field: T,
+}
+
+pub fn change_thing<T>(thing: Thing<T>, t: T) -> Thing<T> {
+  let mut new_field = thing.field;
+  new_field = t;
+  Thing { field: new_field }
 }
 
 pub fn main() {
@@ -13,11 +26,4 @@ pub fn main() {
   assert!(x == 1);
   x = 3;
   assert!(x == 3);
-
-  // Again, local mutation
-  let option = IntOption::Some(2);
-  let mut option2 = IntOption::None;
-  let mut option = IntOption::None;
-  option2 = option;
-  option = IntOption::Some(123);
 }
