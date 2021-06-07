cd84a968947844cb861382bd9023d6ea4de6a105
Yann Bolliger <yann.bolliger@epfl.ch>
2021-06-07 14:47:09 +0200

    Add test with unary and huge tuples.

--- stainless_frontend/tests/pass/tuples.rs
+++ stainless_frontend/tests/pass/tuples.rs
@@ -19,3 +19,26 @@ pub fn abs_pair(x: i32) -> (i32, i32) {
 pub fn swap(t: (i32, i32)) -> (i32, i32) {
   (t.1, t.0)
 }
+
+pub fn main() {
+  #[allow(unused_variables)]
+  let unit_tuple = ();
+
+  let unary_tuple = (123,);
+  assert!(unary_tuple.0 == 123);
+
+  let huge_tuple = (
+    0, "hello", 2, "world", -4, "!", 6, "how", 8, "are", 10, "you", 12, "?",
+  );
+
+  assert!(
+    huge_tuple.0
+      + huge_tuple.2
+      + huge_tuple.4
+      + huge_tuple.6
+      + huge_tuple.8
+      + huge_tuple.10
+      + huge_tuple.12
+      == 34
+  )
+}
